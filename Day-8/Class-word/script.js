53bfd923531ae341eff12ce7297dc836b826db8c
subhankar0002 <172010187+subhankar0002@users.noreply.github.com>
2025-02-21 11:30:37 +0530

    initial commit

---
+++ Day-8/Class-word/script.js
@@ -0,0 +1,27 @@
+// document.getElementById("parent").style.border="2px solid red"
+
+// document.getElementById("parent").style.height="200px"
+
+// document.getElementById("parent").style.backgroundColor=""
+
+// let element = document.createElement("button")
+
+// element.innerText="Submit"
+
+// element.style.backgroundColor="Gray"
+
+// element.style.height="100px 100px"
+
+// document.getElementById("parent").append(element)
+
+
+console.log(variable)
+let variable = 10
+// variable = 15
+// console.log(variable)
+
+function myFunction(){
+    
+    console.log(variable)
+}
+myFunction()
