7470e592aa93700758d1b28131705eb010737a7f
subhankar0002 <172010187+subhankar0002@users.noreply.github.com>
2025-02-21 11:27:52 +0530

    initial commit

---
+++ Day-8/assingment/script.js
@@ -0,0 +1,16 @@
+document.getElementById("a").style.backgroundColor="yellow" //This is Addition Button
+
+
+document.getElementById("d").style.backgroundColor="yellow" //This is Division Button
+
+
+document.getElementById("s").style.backgroundColor="yellow" //This is Substraction Button
+
+
+document.getElementById("m").style.backgroundColor="Yellow" //This is Multiplication Button
+
+
+document.getElementById("e").style.backgroundColor="Green" //This is Equall Button
+
+
+document.getElementById("dot").style.backgroundColor="Gray" //This is Dot Button
