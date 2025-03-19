b27e0bc119b94312e7a01a96c715b5ef43febb07
Ainadri-Mandal <sahasubhankarkoronn@gmail.com>
2025-03-20 00:56:42 +0530

    25th commit

---
+++ Day-25/Assignment/frontend/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        alert("You are already logged in !");
+        return window.location.href = 'dashboard.html';
+    }
+});
