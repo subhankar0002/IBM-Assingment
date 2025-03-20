f0b281c7aa421b1a51d4d4db21b5e04ad266b7c1
Ainadri-Mandal <sahasubhankarkoronn@gmail.com>
2025-03-20 11:41:09 +0530

    Added new files from Day-24 and updated .gitignore

---
+++ Day-24/Assignment/Backend/middleware/authMiddleware.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken');
+const protect = (req, res, next) => {
+ const token = req.header('Authorization');
+ if (!token) return res.status(401).json({ message: 'Access Denied' });
+ try {
+ const decoded = jwt.verify(token, process.env.JWT_SECRET);
+ req.user = decoded;
+ next();
+ } catch (error) {
+    res.status(400).json({ message: 'Invalid Token' });
+ }
+};
+module.exports = {protect};
